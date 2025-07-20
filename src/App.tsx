04fb0bf31b1b4854dcea5d566be803a2ca056a4f
Taciano Junior <t.juniior13@gmail.com>
2025-07-20 10:59:06 -0300

    adicionando components ao app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
 import './Styles/global.css';
 import './Styles/therme.css';
+
 export function App() {
   return (
-    <div>
-      <h1>hello world!</h1>
-    </div>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+    </>
   );
 }
