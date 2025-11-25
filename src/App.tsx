fbcebebf8b1e1bbac068666b7e1dd5d218107a6c
Taciano Junior <t.juniior13@gmail.com>
2025-11-24 21:14:57 -0300

    criando component MainForm, pasta page e pasta template

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,7 @@
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycle } from './components/Cycle';
-import { DefaultButton } from './components/DefaultButton';
-import { Footer } from './components/Footer';
-
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-
+import { Home } from './pages/Home';
 import './Styles/global.css';
 import './Styles/therme.css';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='taskId'
-              labelText='Task'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycle />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
-          </div>
-        </form>
-        <Container>
-          <Footer />
-        </Container>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
